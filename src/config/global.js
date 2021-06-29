771c4334796cca2c9c334dcb5842c1e24b6313a6
Luis Perez <ljpm9401@gmail.com>
2021-06-29 16:38:01 -0500

    ajustes revision

--- src/config/global.js
+++ src/config/global.js
@@ -12,76 +12,84 @@ export default {
         regional: 'Regional Santander',
       },
       {
-        nombre: ['Carlos Andrés Ossa Jiménez', 'José Eduardo Chaves Peña'],
-        cargo: 'Experto temático',
-        centro: 'Centro de Diseño Tecnológico Industrial',
-      },
-      {
         nombre: ['Oscar Riveros'],
         cargo: 'Experto temático',
-        centro: 'Centro Diseño y Metrología',
-      },
-      {
-        nombre: ['Carolina Coca Salazar'],
-        cargo: 'Diseñadora instruccional',
         centro: 'Centro de Diseño y Metrología',
-        regional: 'Regional Distrito Capital',
+        regional: '',
       },
       {
-        nombre: ['Miroslava González Hernández'],
-        cargo: 'Diseñadora y evaluadora instruccional',
-        centro: 'Centro de Gestión Industrial',
-        regional: 'Regional Distrito Capital',
+        nombre: 'José Eduardo Chaves Peña',
+        cargo: 'Experto temático',
+        centro: 'Centro de Diseño Tecnológico Industrial',
+        regional: '',
+      },
+      {
+        nombre: 'Carlos Andrés Ossa Jiménez',
+        cargo: 'Experto temático',
+        centro: 'Centro de Diseño Tecnológico Industrial',
+        regional: '',
+      },
+      {
+        nombre: 'Sergio Arturo Medina Castillo',
+        cargo: 'Diseñador instruccional',
+        centro: 'Centro para la Industria de la Comunicación Gráfica',
+        regional: '',
       },
     ],
     desarrolloProducto: [
       {
         nombre: 'Francisco José Lizcano Reyes',
         cargo: 'Responsable del equipo',
-      },
-      {
-        nombre: [
-          'Luis Fernando Sarmiento Betancourth',
-          'Carlos Julián Ramírez Benítez',
-        ],
-        cargo: 'Diseño web',
-      },
-      {
-        nombre: ['Luis Jesús Pérez Madariaga', 'Lizeth Karina Manchego Suárez'],
-        cargo: 'Desarrollo Front-End',
-      },
-      {
-        nombre: ['Jhana Johanna Bustillo Ardila'],
-        cargo: 'Validación de diseño y contenido',
-      },
-      {
-        nombre: [
-          'Wilson Andrés Arenales Cáceres',
-          'Gilberto Junior Rodríguez Rodríguez',
-          'Víctor Cárdenas',
-          'Daniela Muñoz Bedoya',
-          'Jose Tang',
-          'Ludwyng Corzo',
-          'Lina Pérez',
-          'Ricardo Vásquez',
-        ],
-        cargo: 'Producción audiovisual',
-      },
-      {
         centro: 'Centro Industrial del Diseño y la Manufactura',
         regional: 'Regional Santander',
       },
       {
         nombre: 'Leyson Fabian Castaño Pérez',
         cargo: 'Soporte organizacional',
+        centro: 'Centro de Comercio y Servicios',
+        regional: 'Regional Tolima',
+      },
+      {
+        nombre: 'Carlos Julián Ramírez Benítez',
+        cargo: 'Diseño web',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'Gilberto Junior Rodríguez Rodríguez',
+        cargo: 'Producción audiovisual',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'Víctor Cárdenas',
+        cargo: 'Producción audiovisual',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'Wilson Andrés Arenales Cáceres',
+        cargo: 'Producción audiovisual',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
       },
       {
         nombre: 'Zuleidy María Ruiz Torres',
         cargo: 'Producción audiovisual',
-      },
-      {
         centro: 'Centro de Comercio y Servicios',
         regional: 'Regional Tolima',
+      },
+      {
+        nombre: 'Luis Jesús Pérez Madariaga',
+        cargo: 'Desarrollo front-end',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'María Isabel Román Rueda',
+        cargo: 'Validación de diseño y contenido',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
       },
     ],
     gestoresRepositorio: [
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -14,7 +14,7 @@ section
             ul
               li.mb-3.banner-principal__datos__item
                 p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
-                p.banner-principal__datos__item__texto creación de gemelos digitales de producto en industria 4.0 
+                p.banner-principal__datos__item__texto Creación de gemelos digitales de producto en industria 4.0 
               li.mb-3.banner-principal__datos__item
                 p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
                 p.banner-principal__datos__item__texto 22520109
